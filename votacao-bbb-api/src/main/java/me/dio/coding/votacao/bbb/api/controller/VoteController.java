2f5a91276cfd2f72d16bd0170a501ae0dd8e979d
Cicero Romao <cicerotransformice@gmail.com>
2022-04-14 15:04:09 -0300

    add Vote controller and Service calls to queue kafka

--- votacao-bbb-api/src/main/java/me/dio/coding/votacao/bbb/api/controller/VoteController.java
+++ votacao-bbb-api/src/main/java/me/dio/coding/votacao/bbb/api/controller/VoteController.java
@@ -1,2 +1,25 @@
-package me.dio.coding.votacao.bbb.api.controller;public class VoteController {
+package me.dio.coding.votacao.bbb.api.controller;
+
+import lombok.AllArgsConstructor;
+import me.dio.coding.votacao.bbb.api.model.ParticipantsModel;
+import me.dio.coding.votacao.bbb.api.service.VoteService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/voted")
+@AllArgsConstructor
+public class VoteController {
+
+    private final VoteService service;
+
+    @PostMapping
+    public ResponseEntity<String> vote(@RequestBody ParticipantsModel participant) {
+        service.addEevent(participant);
+        return ResponseEntity.ok("voto computado");
+    }
+
 }
--- votacao-bbb-api/src/main/java/me/dio/coding/votacao/bbb/api/service/VoteService.java
+++ votacao-bbb-api/src/main/java/me/dio/coding/votacao/bbb/api/service/VoteService.java
@@ -1,2 +1,19 @@
-package me.dio.coding.votacao.bbb.api.service;public class VoteService {
+package me.dio.coding.votacao.bbb.api.service;
+
+import lombok.AllArgsConstructor;
+import me.dio.coding.votacao.bbb.api.model.ParticipantsModel;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class VoteService {
+
+    private static final String TOPIC_VOTE = "vote";
+    private final KafkaTemplate<Object, Object> template;
+
+    public void addEevent(ParticipantsModel participantsModel) {
+        template.send(TOPIC_VOTE, participantsModel);
+    }
+
 }
