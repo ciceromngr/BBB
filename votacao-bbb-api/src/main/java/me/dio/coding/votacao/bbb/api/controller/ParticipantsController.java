298cdb8814823770cbb6f83fc1abdb6d42b50973
Cicero Romao <cicerotransformice@gmail.com>
2022-04-14 15:02:55 -0300

    add participants model, controller, repository

--- votacao-bbb-api/src/main/java/me/dio/coding/votacao/bbb/api/controller/ParticipantsController.java
+++ votacao-bbb-api/src/main/java/me/dio/coding/votacao/bbb/api/controller/ParticipantsController.java
@@ -1,2 +1,43 @@
-package me.dio.coding.votacao.bbb.api.controller;public class ParticipantsController {
+package me.dio.coding.votacao.bbb.api.controller;
+
+
+import lombok.AllArgsConstructor;
+import me.dio.coding.votacao.bbb.api.model.ParticipantsModel;
+import me.dio.coding.votacao.bbb.api.repository.ParticipantsRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/participants")
+@AllArgsConstructor
+public class ParticipantsController {
+
+    private final ParticipantsRepository repository;
+
+    @PostMapping("/saving")
+    public ResponseEntity<ParticipantsModel> save(@RequestBody ParticipantsModel participantsModel) {
+        ParticipantsModel saving = repository.save(participantsModel);
+        return ResponseEntity.ok(saving);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<ParticipantsModel>> getAllParticipants() {
+        List<ParticipantsModel> participants = repository.findAll();
+        return ResponseEntity.ok(participants);
+    }
+
+    @GetMapping
+    public ResponseEntity<ParticipantsModel> getParticipantsById(@RequestParam String id) {
+        Optional<ParticipantsModel> opt = repository.findById(id);
+        if(opt.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(opt.get());
+    }
+
+
 }
--- votacao-bbb-api/src/main/java/me/dio/coding/votacao/bbb/api/repository/ParticipantsRepository.java
+++ votacao-bbb-api/src/main/java/me/dio/coding/votacao/bbb/api/repository/ParticipantsRepository.java
@@ -1,2 +1,7 @@
-package me.dio.coding.votacao.bbb.api.repository;public interface ParticipantsRepository {
+package me.dio.coding.votacao.bbb.api.repository;
+
+import me.dio.coding.votacao.bbb.api.model.ParticipantsModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ParticipantsRepository extends MongoRepository<ParticipantsModel, String> {
 }
